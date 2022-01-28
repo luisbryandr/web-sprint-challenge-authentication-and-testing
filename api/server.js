ba531053ac1269589997bf060671408b91a8aafa
luisbryandr <luisbdominguezr@gmail.com>
2022-01-28 18:45:36 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -16,4 +16,19 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+server.use('/', (req, res) => {
+    res.json({message: 'Server running'})
+    console.log('api works')
+    
+})
+
+server.use((err, req, res, next) => {//eslint-disable-line  
+    res.status(err.status || 500).json({
+        message:err.message,
+        stack: err.stack,
+    })
+
+}) 
+
 module.exports = server;
+

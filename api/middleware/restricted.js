713e59f9f3504947a90e74b31daba605eb80e767
luisbryandr <luisbdominguezr@gmail.com>
2022-01-28 19:45:58 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,23 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secrets/index')
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'token invalid' })
+      } else {
+        req.decodedJwt = decoded
+        next()
+      }
+    })
+  } else {
+    res.status(407).json({ message: 'token required' })
+  }
+}
+
   /*
     IMPLEMENT
 
@@ -11,4 +29,3 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};

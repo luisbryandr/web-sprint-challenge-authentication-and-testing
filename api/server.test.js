c263583d7e446b1eeaf2d8421d01f792c17c45cf
luisbryandr <luisbdominguezr@gmail.com>
2022-01-28 19:51:45 -0500

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,51 @@
 // Write your tests here
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require('./server')
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+const newUser = {username: "test", password: "1234"};
+
+describe('[POST] /api/auth/register', () => {
+  it('returns a 201 created status on successful registration', async () => {
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send(newUser);
+    expect(res.status).toBe(201);
+  });
+  
+  it('responds with the newly registered user', async () => {
+    let res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'test2', password: '1234' })
+    expect(res.body).toMatchObject({ id: 2, username: 'test2' })
+  })
+});
+
+describe('[POST] /api/auth/login', () => {
+  it('responds with a 200 OK status', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send(newUser)
+    expect(res.status).toBe(200)
+  })
+
+  it('responds with a 422 if no username or password in payload', async () => {
+    let res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: '', password: '1234' })
+    expect(res.status).toBe(422)
+  })
 })
